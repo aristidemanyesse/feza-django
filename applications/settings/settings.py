4b060f0ce7f9c97e05e05a168b18e3ee65dee929
aristide.manyesse@jool-int.com <aristide.manyesse@jool-int.com>
2023-06-12 12:40:18 +0000

    ..

--- applications/settings/settings.py
+++ applications/settings/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     "django.contrib.gis",
     "leaflet",
     'graphene_gis',
@@ -153,7 +153,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR , "media"),
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "static"),
