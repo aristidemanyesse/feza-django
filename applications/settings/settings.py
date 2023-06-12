8270283570d713a3f3bc4957e42cce4cf74c62db
aristide.manyesse@jool-int.com <aristide.manyesse@jool-int.com>
2023-06-12 12:48:30 +0000

    ...

--- applications/settings/settings.py
+++ applications/settings/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     "django.contrib.gis",
     "leaflet",
     'graphene_gis',
@@ -155,9 +155,6 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR , "static"),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
